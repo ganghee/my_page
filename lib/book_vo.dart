9fe7ae04f05f4c924f6e3b3a118a1133c6e4714c
ganghee <idiosauri@naver.com>
2025-02-23 23:31:57 +0900

    create BookVo class

---
+++ lib/book_vo.dart
@@ -0,0 +1,102 @@
+class BookVo {
+  String title;
+  String author;
+  String publisher;
+  String coverImage;
+  String description;
+
+  BookVo(
+    this.title,
+    this.author,
+    this.publisher,
+    this.coverImage,
+    this.description,
+  );
+}
+
+List<BookVo> myBooks = [
+  BookVo(
+    '플러터 인 액션1',
+    '에릭 윈드밀',
+    '맨닝',
+    'https://books.google.co.kr/books/publisher/content?id=NcjLEAAAQBAJ&pg=PP1&img=1&zoom=3&hl=en&bul=1&sig=ACfU3U1TkjpTSBZrpIOtFQMpznI2mECo_g&w=1280',
+    '플러터를 사용하여 iOS와 안드로이드 앱을 만드는 방법을 알려줍니다.',
+  ),
+  BookVo(
+    '플러터 인 액션2',
+    '에릭 윈드밀',
+    '맨닝',
+    'https://books.google.co.kr/books/publisher/content?id=NcjLEAAAQBAJ&pg=PP1&img=1&zoom=3&hl=en&bul=1&sig=ACfU3U1TkjpTSBZrpIOtFQMpznI2mECo_g&w=1280',
+    '플러터를 사용하여 iOS와 안드로이드 앱을 만드는 방법을 알려줍니다.',
+  ),
+  BookVo(
+    '플러터 인 액션3',
+    '에릭 윈드밀',
+    '맨닝',
+    'https://books.google.co.kr/books/publisher/content?id=NcjLEAAAQBAJ&pg=PP1&img=1&zoom=3&hl=en&bul=1&sig=ACfU3U1TkjpTSBZrpIOtFQMpznI2mECo_g&w=1280',
+    '플러터를 사용하여 iOS와 안드로이드 앱을 만드는 방법을 알려줍니다.',
+  ),
+  BookVo(
+    '플러터 인 액션4',
+    '에릭 윈드밀',
+    '맨닝',
+    'https://books.google.co.kr/books/publisher/content?id=NcjLEAAAQBAJ&pg=PP1&img=1&zoom=3&hl=en&bul=1&sig=ACfU3U1TkjpTSBZrpIOtFQMpznI2mECo_g&w=1280',
+    '플러터를 사용하여 iOS와 안드로이드 앱을 만드는 방법을 알려줍니다.',
+  ),
+  BookVo(
+    '플러터 인 액션5',
+    '에릭 윈드밀',
+    '맨닝',
+    'https://books.google.co.kr/books/publisher/content?id=NcjLEAAAQBAJ&pg=PP1&img=1&zoom=3&hl=en&bul=1&sig=ACfU3U1TkjpTSBZrpIOtFQMpznI2mECo_g&w=1280',
+    '플러터를 사용하여 iOS와 안드로이드 앱을 만드는 방법을 알려줍니다.',
+  ),
+  BookVo(
+    '플러터 인 액션6',
+    '에릭 윈드밀',
+    '맨닝',
+    'https://books.google.co.kr/books/publisher/content?id=NcjLEAAAQBAJ&pg=PP1&img=1&zoom=3&hl=en&bul=1&sig=ACfU3U1TkjpTSBZrpIOtFQMpznI2mECo_g&w=1280',
+    '플러터를 사용하여 iOS와 안드로이드 앱을 만드는 방법을 알려줍니다.',
+  ),
+  BookVo(
+    '플러터 인 액션7',
+    '에릭 윈드밀',
+    '맨닝',
+    'https://books.google.co.kr/books/publisher/content?id=NcjLEAAAQBAJ&pg=PP1&img=1&zoom=3&hl=en&bul=1&sig=ACfU3U1TkjpTSBZrpIOtFQMpznI2mECo_g&w=1280',
+    '플러터를 사용하여 iOS와 안드로이드 앱을 만드는 방법을 알려줍니다.',
+  ),
+  BookVo(
+    '플러터 인 액션8',
+    '에릭 윈드밀',
+    '맨닝',
+    'https://books.google.co.kr/books/publisher/content?id=NcjLEAAAQBAJ&pg=PP1&img=1&zoom=3&hl=en&bul=1&sig=ACfU3U1TkjpTSBZrpIOtFQMpznI2mECo_g&w=1280',
+    '플러터를 사용하여 iOS와 안드로이드 앱을 만드는 방법을 알려줍니다.',
+  ),
+  BookVo(
+    '플러터 인 액션9',
+    '에릭 윈드밀',
+    '맨닝',
+    'https://books.google.co.kr/books/publisher/content?id=NcjLEAAAQBAJ&pg=PP1&img=1&zoom=3&hl=en&bul=1&sig=ACfU3U1TkjpTSBZrpIOtFQMpznI2mECo_g&w=1280',
+    '플러터를 사용하여 iOS와 안드로이드 앱을 만드는 방법을 알려줍니다.',
+  ),
+  BookVo(
+    '플러터 인 액션10',
+    '에릭 윈드밀',
+    '맨닝',
+    'https://books.google.co.kr/books/publisher/content?id=NcjLEAAAQBAJ&pg=PP1&img=1&zoom=3&hl=en&bul=1&sig=ACfU3U1TkjpTSBZrpIOtFQMpznI2mECo_g&w=1280',
+    '플러터를 사용하여 iOS와 안드로이드 앱을 만드는 방법을 알려줍니다.',
+  ),
+  BookVo(
+    '플러터 인 액션11',
+    '에릭 윈드밀',
+    '맨닝',
+    'https://books.google.co.kr/books/publisher/content?id=NcjLEAAAQBAJ&pg=PP1&img=1&zoom=3&hl=en&bul=1&sig=ACfU3U1TkjpTSBZrpIOtFQMpznI2mECo_g&w=1280',
+    '플러터를 사용하여 iOS와 안드로이드 앱을 만드는 방법을 알려줍니다.',
+  ),
+  BookVo(
+    '플러터 인 액션12',
+    '에릭 윈드밀',
+    '맨닝',
+    'https://books.google.co.kr/books/publisher/content?id=NcjLEAAAQBAJ&pg=PP1&img=1&zoom=3&hl=en&bul=1&sig=ACfU3U1TkjpTSBZrpIOtFQMpznI2mECo_g&w=1280',
+    '플러터를 사용하여 iOS와 안드로이드 앱을 만드는 방법을 알려줍니다.',
+  ),
+];
