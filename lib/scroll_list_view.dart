4ad0f3d46f7c9e902a418ee46360f4f12d1747dc
ganghee <idiosauri@naver.com>
2025-03-14 23:14:56 +0900

    refactor vertical scroll

--- lib/scroll_list_view.dart
+++ lib/scroll_list_view.dart
@@ -3,12 +3,11 @@ import 'dart:math';
 import 'package:collection/collection.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:my/baking/baking_view.dart';
 
 import 'book_vo.dart';
 
 part 'books_view.dart';
-
-part 'baking_view.dart';
 
 class ScrollListView extends StatefulWidget {
   const ScrollListView({super.key});
@@ -24,15 +23,15 @@ enum ScrollState {
 }
 
 class _ScrollListViewState extends State<ScrollListView> {
-  final ScrollController _verticalController = ScrollController();
-  final ScrollController _bookScrollController = ScrollController();
+  late final ScrollController _verticalController = ScrollController();
+  late final ScrollController _bookScrollController = ScrollController();
   final GlobalKey _horizontalKey = GlobalKey();
   final horizontalScrollSpeed = 0.5;
   double _horizontalProgress = 0.0;
-  bool _isVerticalScrollable = true;
   double _horizontalY = 0; // 가로 스크롤 위치
   double _horizontalHeight = 0; // 가로 스크롤 높이
   ScrollState _isHorizontalScrollState = ScrollState.init;
+  final ValueNotifier<bool> _isVerticalScrollable = ValueNotifier<bool>(true);
 
   @override
   void initState() {
@@ -53,29 +52,36 @@ class _ScrollListViewState extends State<ScrollListView> {
         /// detect scroll event
         await detectBookScroll(pointerSignal);
       },
-      child: SingleChildScrollView(
-        controller: _verticalController,
-        physics:
-            _isVerticalScrollable ? null : const NeverScrollableScrollPhysics(),
-        child: Column(
-          children: List.generate(4, (index) {
-            if (index == 0) {
-              return bakingView();
-            } else if (index == 1) {
-              return booksView(
-                screenHeight: MediaQuery.of(context).size.height,
-                horizontalKey: _horizontalKey,
-                bookScrollController: _bookScrollController,
-              );
-            } else {
-              return Container(
-                width: MediaQuery.of(context).size.width,
-                height: MediaQuery.of(context).size.height,
-                color: Colors.blue,
-                child: Text("안녕하세요"),
-              );
-            }
-          }),
+      child: ValueListenableBuilder(
+        valueListenable: _isVerticalScrollable,
+        builder: (
+          BuildContext context,
+          bool isScrollable,
+          Widget? child,
+        ) =>
+            SingleChildScrollView(
+          controller: _verticalController,
+          physics: isScrollable ? null : const NeverScrollableScrollPhysics(),
+          child: Column(
+            children: List.generate(4, (index) {
+              if (index == 0) {
+                return bakingView();
+              } else if (index == 1) {
+                return booksView(
+                  screenHeight: MediaQuery.of(context).size.height,
+                  horizontalKey: _horizontalKey,
+                  bookScrollController: _bookScrollController,
+                );
+              } else {
+                return Container(
+                  width: MediaQuery.of(context).size.width,
+                  height: MediaQuery.of(context).size.height,
+                  color: Colors.blue,
+                  child: Text("안녕하세요"),
+                );
+              }
+            }),
+          ),
         ),
       ),
     );
@@ -83,7 +89,7 @@ class _ScrollListViewState extends State<ScrollListView> {
 
   // 스크롤 위치를 고정시키는 함수
   fixScrollPosition() async {
-    _isVerticalScrollable = false;
+    _isVerticalScrollable.value = false;
     _verticalController.jumpTo(_horizontalY);
     await Future.delayed(Duration(milliseconds: 5)); // 5ms 딜레이를 줘야 정상적으로 동작함
     _verticalController.jumpTo(_horizontalY);
@@ -94,41 +100,42 @@ class _ScrollListViewState extends State<ScrollListView> {
     if (pointerSignal is PointerScrollEvent) {
       final currentPosition = _verticalController.offset; // 현재 위치
       // 스크롤 위치가 가로 스크롤 위치에 있을 때
-      if (currentPosition >= _horizontalY &&
+      if (pointerSignal.scrollDelta.dy > 0 &&
+          currentPosition >= _horizontalY &&
           currentPosition < _horizontalY + _horizontalHeight) {
         // 아래로 스크롤 시
-        if (pointerSignal.scrollDelta.dy > 0) {
-          if (_isHorizontalScrollState == ScrollState.init) {
-            _isHorizontalScrollState = ScrollState.scrolling;
-          }
-          if (_isHorizontalScrollState == ScrollState.scrolling) {
-            _horizontalProgress +=
-                pointerSignal.scrollDelta.dy * horizontalScrollSpeed;
-            await fixScrollPosition();
-            if (_horizontalProgress < 0) {
-              _horizontalProgress = 0;
-            }
+        if (_isHorizontalScrollState == ScrollState.init) {
+          _isHorizontalScrollState = ScrollState.scrolling;
+        }
+        if (_isHorizontalScrollState == ScrollState.scrolling) {
+          _horizontalProgress +=
+              pointerSignal.scrollDelta.dy * horizontalScrollSpeed;
 
-            if (_horizontalProgress >
-                _bookScrollController.position.maxScrollExtent - 10) {
-              _isVerticalScrollable = true;
-              _isHorizontalScrollState = ScrollState.end;
-            }
-            setState(() {});
-          }
-        } else {
-          if (_isHorizontalScrollState == ScrollState.scrolling) {
+          if (_isVerticalScrollable.value) {
             await fixScrollPosition();
-            _horizontalProgress +=
-                pointerSignal.scrollDelta.dy * horizontalScrollSpeed;
-            // 가로 스크롤이 초기 상태로 되돌아 왔을 때
-            if (_horizontalProgress < 0) {
-              _isVerticalScrollable = true;
-              _horizontalProgress = 0;
-              _isHorizontalScrollState = ScrollState.init;
-            }
           }
-          setState(() {});
+          if (_horizontalProgress < 0) {
+            _horizontalProgress = 0;
+          }
+
+          if (_horizontalProgress >
+              _bookScrollController.position.maxScrollExtent - 10) {
+            _isVerticalScrollable.value = true;
+            _isHorizontalScrollState = ScrollState.end;
+          }
+        }
+      } else if ((pointerSignal.scrollDelta.dy < 0 &&
+          currentPosition <= _horizontalY)) {
+        // 위로 스크롤 시
+        if (_isHorizontalScrollState == ScrollState.scrolling) {
+          _horizontalProgress +=
+              pointerSignal.scrollDelta.dy * horizontalScrollSpeed;
+          // 가로 스크롤이 초기 상태로 되돌아 왔을 때
+          if (_horizontalProgress < 0) {
+            _isVerticalScrollable.value = true;
+            _horizontalProgress = 0;
+            _isHorizontalScrollState = ScrollState.init;
+          }
         }
       }
       if (currentPosition < _horizontalY &&
