0d9681e97fe705a258b9b5bc5f4e72a67b3e273f
ganghee <idiosauri@naver.com>
2025-03-23 16:10:54 +0900

    change baking colors

--- lib/baking/baking_vo.dart
+++ lib/baking/baking_vo.dart
@@ -38,7 +38,7 @@ List<BakingVo> bakings = [
       '반죽을 삼각형 모양으로 자르고 말아서 따뜻한 곳에서 30분 발효',
       '발효된 반죽에 계란을 바르고 200도로 13분간 굽기',
     ],
-    color: Colors.yellow,
+    color: Colors.orange.shade50,
   ),
   BakingVo(
     name: '식빵',
@@ -47,7 +47,7 @@ List<BakingVo> bakings = [
     ingredients:
         '우유 140g, 계란 35g, 설탕 35g, 소금 6g, 드라이 이스트 4g, 강력분 240g, 무염 버터 32g',
     recipe: [],
-    color: Colors.blue,
+    color: Colors.blue.shade50,
   ),
   BakingVo(
     name: '모닝빵',
@@ -56,7 +56,7 @@ List<BakingVo> bakings = [
     ingredients:
         '우유 160g, 계란 하나, 설탕 40g, 소금 5g, 드라이 이스트 5g, 강력분 310g, 무염 버터 35g',
     recipe: [],
-    color: Colors.green,
+    color: Colors.green.shade50,
   ),
   BakingVo(
     name: '스콘',
@@ -64,7 +64,7 @@ List<BakingVo> bakings = [
         'https://orchardleafoods.co.uk/wp-content/uploads/2023/02/Scone_City.png',
     ingredients: '박력분 200g, 베이킹파우더 5g, 소금 2g, 설탕 35g, 무염 버터 80g, 우유 25g, 계란 1개',
     recipe: [],
-    color: Colors.pinkAccent,
+    color: Colors.grey.shade200,
   ),
   BakingVo(
     name: '쿠키',
@@ -73,7 +73,7 @@ List<BakingVo> bakings = [
     ingredients:
         '차가운 무염 버터 94g, 설탕 114g, 중력분 155g, 베이킹 파우더 2g, 소금 1g, 계란 1개, 베이킹소다 2g, 전처리한 호두 150g, 다진 초콜릿 168g',
     recipe: [],
-    color: Colors.white,
+    color: Colors.brown.shade200,
   ),
   BakingVo(
     name: '치즈 케이크',
@@ -81,7 +81,8 @@ List<BakingVo> bakings = [
         'https://images.squarespace-cdn.com/content/v1/538500e4e4b0fa9e95efc7b9/1627656601170-ZPCM9P1F97Y0XZU1FVEB/New+York+Baby+CC+slice+isolated.png?format=1000w',
     ingredients: '크림 치즈 350g, 설탕 80g, 계란 85g, 생크림 110g',
     recipe: [],
-    color: Colors.greenAccent,
+    color: Colors.yellow.shade50
+    ,
   ),
   BakingVo(
     name: '두바이 초콜릿',
@@ -89,6 +90,6 @@ List<BakingVo> bakings = [
         'https://www.mochidoh.ca/cdn/shop/files/dubai_bar_no_background.png?v=1739144773',
     ingredients: '카다이프 200g, 버터 110g, 피스타치오 페이스트 500g, 커버춰 400g',
     recipe: [],
-    color: Colors.white30,
+    color: Colors.green.shade50,
   ),
 ];
