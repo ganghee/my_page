1b4b1a1b9356624fcaa1995d5c5b0b149cf492df
ganghee <idiosauri@naver.com>
2025-04-28 20:43:35 +0900

    set bangkok data

--- lib/travel/model/bangkok_vo.dart
+++ lib/travel/model/bangkok_vo.dart
@@ -3,65 +3,400 @@ part of 'travel_detail_vo.dart';
 final TravelDetailVo _bangkokVo = TravelDetailVo(
   travelId: 1,
   items: [
-    TravelItemVo(type: TravelDetailType.text, description: '방콕 여행'),
     TravelItemVo(
-      type: TravelDetailType.image,
-      description:
-          'https://blog-static.kkday.com/ko/blog/wp-content/uploads/%EB%B0%A9%EC%BD%95-%EC%99%95%EA%B6%81-%EC%A0%84%EA%B2%BD.jpg',
+      type: TravelDetailType.text,
+      description: '2024년 12월 15일부터 12월 20일까지 4박 6일동안 방콕 여행을 다녀왔어요.\n'
+          '방콕은 태국의 수도로, 동남아시아에서 가장 인기 있는 여행지 중 하나입니다.\n'
+          '방콕은 다양한 문화와 역사, 맛있는 음식, 아름다운 경치 등으로 유명합니다.\n'
+          '\n'
+          '인천 공항에서 10시 40분 비행기였지만 대기 줄이 너무 길어서 비행기를 놓쳤습니다...\n'
+          '다행이 다음 4시 반 비행기 자리가 있어서 다시 예약해서 비행이를 탔습니다.\n'
+          '아까운 내 돈...\n'
+          '인천 공항에서 6시간 걸려 돈므앙 공항에 도착 했습니다\n',
+      descriptionEnglish:
+          'From December 15 to December 20, 2024, I went on a 4-night, 6-day trip to Bangkok.\n'
+          'Bangkok is the capital of Thailand and one of the most popular travel destinations in Southeast Asia.\n'
+          'Bangkok is famous for its diverse culture and history, delicious food, and beautiful scenery.\n'
+          '\n'
+          'I had a flight at 10:40 AM from Incheon Airport, but I missed it because the waiting line was too long...\n'
+          'Fortunately, there was a seat available on the next flight at 4:30 PM, so I rebooked and boarded the plane.\n'
+          'My precious money...\n'
+          'It took 6 hours from Incheon Airport to arrive at Don Mueang Airport.',
     ),
-    TravelItemVo(type: TravelDetailType.text, description: '방콕 여행2'),
     TravelItemVo(
       type: TravelDetailType.image,
-      description:
-          'https://blog-static.kkday.com/ko/blog/wp-content/uploads/%EB%B0%A9%EC%BD%95-%EC%99%95%EA%B6%81-%EC%A0%84%EA%B2%BD.jpg',
+      imageUrls: [
+        'assets/images/bangkok/bangkok-1-waiting.webp',
+        'assets/images/bangkok/airport.webp',
+      ],
     ),
-    TravelItemVo(type: TravelDetailType.text, description: '방콕 여행3'),
     TravelItemVo(
-      type: TravelDetailType.image,
-      description:
-          'https://blog-static.kkday.com/ko/blog/wp-content/uploads/%EB%B0%A9%EC%BD%95-%EC%99%95%EA%B6%81-%EC%A0%84%EA%B2%BD.jpg',
+      type: TravelDetailType.text,
+      description: '공항에서 버스를 타고 야속역 근처의 숙소로 이동했어요.\n'
+          '저녁은 그랩 어플로 망고밥과 쏨땀 새우볶음밥을 배달해먹었어요.\n'
+          '망고밥은 단 맛이 강하고 쏨땀은 매운맛이 강해요.\n'
+          '처음 먹어보는 맛이라 새로웠어요\n',
+      descriptionEnglish:
+          'I took a bus from the airport to my accommodation near Yao Sok Station.\n'
+          'For dinner, I ordered mango rice and som tam shrimp fried rice through the Grab app.\n'
+          'Mango rice has a strong sweetness, while som tam is very spicy.\n'
+          'It was a new taste for me.',
     ),
-    TravelItemVo(type: TravelDetailType.text, description: '방콕 여행2'),
     TravelItemVo(
       type: TravelDetailType.image,
-      description:
-          'https://blog-static.kkday.com/ko/blog/wp-content/uploads/%EB%B0%A9%EC%BD%95-%EC%99%95%EA%B6%81-%EC%A0%84%EA%B2%BD.jpg',
+      imageUrls: [
+        'assets/images/bangkok/delivery.webp',
+      ],
     ),
-    TravelItemVo(type: TravelDetailType.text, description: '방콕 여행5'),
     TravelItemVo(
-      type: TravelDetailType.image,
-      description:
-          'https://blog-static.kkday.com/ko/blog/wp-content/uploads/%EB%B0%A9%EC%BD%95-%EC%99%95%EA%B6%81-%EC%A0%84%EA%B2%BD.jpg',
+      type: TravelDetailType.text,
+      description: '다음날 둘 째날 16일 오전에 아유타야 투어를 갔어요.\n'
+          '아유타야는 방콕에서 북쪽으로 80km 떨어진 곳에 위치한 곳이예요.\n'
+          '미얀마의 침입으로 많이 손실 되었으나 유적들이 굉장히 많은 곳이였어요.\n',
+      descriptionEnglish:
+          'On the second day, December 16th, I went on a tour to Ayutthaya.\n'
+          'Ayutthaya is located 80 km north of Bangkok.\n'
+          'Although it suffered significant damage from the invasion of Myanmar, it is a place with many ruins.',
     ),
-    TravelItemVo(type: TravelDetailType.text, description: '방콕 여행2'),
     TravelItemVo(
       type: TravelDetailType.image,
-      description:
-          'https://blog-static.kkday.com/ko/blog/wp-content/uploads/%EB%B0%A9%EC%BD%95-%EC%99%95%EA%B6%81-%EC%A0%84%EA%B2%BD.jpg',
+      imageUrls: [
+        'assets/images/bangkok/ayotaya_1.webp',
+      ],
     ),
-    TravelItemVo(type: TravelDetailType.text, description: '방콕 7'),
     TravelItemVo(
       type: TravelDetailType.image,
-      description:
-          'https://blog-static.kkday.com/ko/blog/wp-content/uploads/%EB%B0%A9%EC%BD%95-%EC%99%95%EA%B6%81-%EC%A0%84%EA%B2%BD.jpg',
+      imageUrls: [
+        'assets/images/bangkok/ayotaya_2.webp',
+        'assets/images/bangkok/ayotaya_3.webp',
+      ],
     ),
-    TravelItemVo(type: TravelDetailType.text, description: '방콕 여행8'),
     TravelItemVo(
       type: TravelDetailType.image,
-      description:
-          'https://blog-static.kkday.com/ko/blog/wp-content/uploads/%EB%B0%A9%EC%BD%95-%EC%99%95%EA%B6%81-%EC%A0%84%EA%B2%BD.jpg',
+      imageUrls: [
+        'assets/images/bangkok/ayotaya_4.webp',
+        'assets/images/bangkok/ayotaya_5.webp',
+      ],
     ),
-    TravelItemVo(type: TravelDetailType.text, description: '방콕 여행9'),
     TravelItemVo(
       type: TravelDetailType.image,
-      description:
-          'https://blog-static.kkday.com/ko/blog/wp-content/uploads/%EB%B0%A9%EC%BD%95-%EC%99%95%EA%B6%81-%EC%A0%84%EA%B2%BD.jpg',
+      imageUrls: [
+        'assets/images/bangkok/ayotaya_6.webp',
+        'assets/images/bangkok/ayotaya_7.webp',
+      ],
     ),
-    TravelItemVo(type: TravelDetailType.text, description: '방콕 여행10'),
+    TravelItemVo(
+      type: TravelDetailType.text,
+      description: '투어를 끝내고 룸피니 공원 근처에서 점심을 먹었어요\n'
+          '폴로 프라이드 식당이라는 곳에서 쏨땀과 항정살 맥주를 시켜 먹었습니다.\n',
+      descriptionEnglish: 'After the tour, I had lunch near Lumpini Park.\n'
+          'I ordered som tam and pork neck beer at a restaurant called Polo Pride.\n'
+          'Som tam is a spicy papaya salad, and the pork neck was tender and flavorful.\n'
+          'I also tried some local beers, which were refreshing and light.',
+    ),
     TravelItemVo(
       type: TravelDetailType.image,
-      description:
-          'https://blog-static.kkday.com/ko/blog/wp-content/uploads/%EB%B0%A9%EC%BD%95-%EC%99%95%EA%B6%81-%EC%A0%84%EA%B2%BD.jpg',
+      imageUrls: [
+        'assets/images/bangkok/polopride.webp',
+      ],
+    ),
+    TravelItemVo(
+      type: TravelDetailType.text,
+      description: '식사 후에 룸피니 공원에서 산책을 했어요.\n'
+          '가는 날이 장날인지 공원에서 축제를 하고 있었어요.\n'
+          '다양한 음식과 디저트를 팔고 있었어요.\n'
+          '감자 과자와 닭 국수를 사먹었어요.\n'
+          '과자는 짜지 않고 보통 과자보다 얇은 칩으로 바삭바삭하고 고소했어요.\n'
+          '닭 국수는 태국 음식에서 많이 나는 단맛과 한약 맛이 강했어요.\n'
+          '호수에서 도마뱀이 헤엄치고 있는 것을 볼 수 있었어요.',
+      descriptionEnglish: 'After the meal, I took a walk in Lumpini Park.\n'
+          'Coincidentally, there was a festival happening in the park.\n'
+          'They were selling various foods and desserts.\n'
+          'I bought some potato chips and chicken noodles.\n'
+          'The chips were not salty and were thinner than regular chips, making them crispy and nutty.\n'
+          'The chicken noodles had a strong sweetness and herbal taste, which is common in Thai cuisine.\n'
+          'I could see monitor lizards swimming in the lake.',
+    ),
+    TravelItemVo(
+      type: TravelDetailType.image,
+      imageUrls: [
+        'assets/images/bangkok/roompini_1.webp',
+        'assets/images/bangkok/roompini_2.webp',
+      ],
+    ),
+    TravelItemVo(
+      type: TravelDetailType.image,
+      imageUrls: [
+        'assets/images/bangkok/roompini_3.webp',
+        'assets/images/bangkok/roompini_4.webp',
+      ],
+    ),
+    TravelItemVo(
+      type: TravelDetailType.image,
+      imageUrls: [
+        'assets/images/bangkok/roompini_5.webp',
+        'assets/images/bangkok/roompini_6.webp',
+      ],
+    ),
+    TravelItemVo(
+      type: TravelDetailType.text,
+      description: '저녁에는 쩟 페어 시장에 갔었어요.\n'
+          '사람들이 굉장히 많았고 노상 형태로 식당들과 소품점들이 많았어요.\n'
+          '랭샙을 먹었어요. 태국의 돼지찜 같은 건데 맛은 똠양꿍 맛이 였어요.\n'
+          '그래서 매운 맛과 신 맛이 강했어요.\n'
+          '태국에서 유명하다는 로티를 먹었어요.\n'
+          '태국식 팬케이크 같은 건데 밀가루 반죽에 바나나를 넣고 구워요.\n'
+          '그 위에 초콜릿 연유를 뿌려서 굉장히 달달했어요.\n',
+      descriptionEnglish: 'In the evening, I went to the Chatuchak market.\n'
+          'There were a lot of people, and it was a street-style market with many restaurants and souvenir shops.\n'
+          'I tried "Larb," which is similar to Thai-style pork stew, but it tasted like Tom Yum Goong.\n'
+          'It had a strong spicy and sour flavor.\n'
+          'I also tried "Roti," which is a famous Thai dessert.\n'
+          'It’s like a Thai pancake made with flour dough and filled with bananas, topped with chocolate syrup, making it very sweet.\n',
+    ),
+    TravelItemVo(
+      type: TravelDetailType.image,
+      imageUrls: [
+        'assets/images/bangkok/jjeokpair_1.webp',
+        'assets/images/bangkok/jjeokpair_2.webp',
+      ],
+    ),
+    TravelItemVo(
+      type: TravelDetailType.image,
+      imageUrls: [
+        'assets/images/bangkok/jjeokpair_3.webp',
+      ],
+    ),
+    TravelItemVo(
+      type: TravelDetailType.text,
+      description: '셋 째날 17일에는 아이콘 시암에 갔었어요.\n'
+          '아이콘 시암은 방콕에서 가장 큰 쇼핑몰 중 하나로, 다양한 브랜드와 레스토랑이 있어요.\n'
+          '쇼핑몰 안에는 태국 전통 문화와 현대적인 디자인이 조화를 이루고 있어요.\n'
+          '아이콘 시암에서 볶음밥과 삼겹살 덮밥, 악어고기 타이 티를 먹었어요.\n'
+          '악어고기는 고소하고 쫄깃한 식감이 좋았어요.\n'
+          '타이 티는 태국식 밀크티로 홍차 맛이 강하고 달달했어요.\n',
+      descriptionEnglish:
+          'On the third day, December 17th, I went to Icon Siam.\n'
+          'Icon Siam is one of the largest shopping malls in Bangkok, featuring various brands and restaurants.\n'
+          'Inside the mall, there is a blend of traditional Thai culture and modern design.\n'
+          'I had fried rice, pork belly rice bowl, and crocodile meat Thai tea.\n'
+          'The crocodile meat was delicious with a nutty and chewy texture.\n'
+          'Thai tea is a Thai-style milk tea with a strong black tea flavor and sweetness.',
+    ),
+    TravelItemVo(
+      type: TravelDetailType.image,
+      imageUrls: [
+        'assets/images/bangkok/iconsiam_1.webp',
+      ],
+    ),
+    TravelItemVo(
+      type: TravelDetailType.text,
+      description: '왓 아룬과 왓 포 사원으로 갔어요\n'
+          '왓 아룬은 태국의 대표적인 사원 중 하나로, 아름다운 경치와 독특한 건축물로 유명해요.\n'
+          '왓 포는 태국에서 가장 큰 와불상이 있었어요.\n'
+          '사원 내부에는 다양한 불상과 조각들이 있어요.\n',
+      descriptionEnglish: 'I visited Wat Arun and Wat Pho temples.\n'
+          'Wat Arun is one of Thailand\'s iconic temples, known for its beautiful scenery and unique architecture.\n'
+          'Wat Pho is home to the largest reclining Buddha statue in Thailand.\n'
+          'Inside the temple, there are various Buddha statues and sculptures.',
+    ),
+    TravelItemVo(
+      type: TravelDetailType.image,
+      imageUrls: [
+        'assets/images/bangkok/whatpho_1.webp',
+      ],
+    ),
+    TravelItemVo(
+      type: TravelDetailType.image,
+      imageUrls: [
+        'assets/images/bangkok/whatpho_2.webp',
+      ],
+    ),
+    TravelItemVo(
+      type: TravelDetailType.text,
+      description: '저녁에는 왓 포 근처에 있는 촘 아룬에서 저녁을 먹었어요.\n'
+          '모닝글로리와 푸팟퐁커리, 크리스피롤을 먹었어요.\n'
+          '모닝글로리는 태국식 시금치 볶음으로, 고소하고 아삭한 식감이 좋았어요.\n'
+          '푸팟퐁커리는 태국식 커리로, 카레와 단 맛이 조화를 이루고 있었어요.\n'
+          '크리스피롤은 바삭한 튀김으로, 속에는 새우가 있었어요.\n',
+      descriptionEnglish:
+          'I had dinner at Chom Arun, a restaurant near Wat Pho.\n'
+          'I ordered morning glory, Phat Phong curry, and crispy rolls.\n'
+          'Morning glory is a Thai-style stir-fried spinach, which was nutty and crunchy.\n'
+          'Phat Phong curry is a Thai-style curry with a blend of curry and sweetness.\n'
+          'Crispy rolls are deep-fried snacks with shrimp inside.',
+    ),
+    TravelItemVo(
+      type: TravelDetailType.image,
+      imageUrls: [
+        'assets/images/bangkok/chromarun_1.webp',
+      ],
+    ),
+    TravelItemVo(
+      type: TravelDetailType.text,
+      description: '저녁을 먹고 키오산 로드로 갔어요.\n'
+          '키오산 로드는 방콕의 유명한 관광지 중 하나로, 다양한 음식과 술집, 상점들이 있어요.\n'
+          '사람들이 굉장히 많았고 음악 소리가 굉장히 컸어요.\n',
+      descriptionEnglish: 'After dinner, I went to Khao San Road.\n'
+          'Khao San Road is one of Bangkok\'s famous tourist spots, known for its variety of food, bars, and shops.\n'
+          'There were a lot of people, and the music was very loud.',
+    ),
+    TravelItemVo(
+      type: TravelDetailType.text,
+      description: '넷 째날 18일에는 파타야로 갔어요.\n'
+          '버스를 타고 2시간 10분을 이동했어요.\n'
+          '버스에서 내리고 택시를 타고 한식당으로 갔어요.\n'
+          '뚝배기 불고기와 비빔밥을 시켜 먹었어요.\n',
+      descriptionEnglish:
+          'On the fourth day, December 18th, I went to Pattaya.\n'
+          'I took a bus for 2 hours and 10 minutes.\n'
+          'After getting off the bus, I took a taxi to a Korean restaurant.\n'
+          'I ordered hot stone bulgogi and bibimbap.',
+    ),
+    TravelItemVo(
+      type: TravelDetailType.image,
+      imageUrls: [
+        'assets/images/bangkok/korean_food_1.webp',
+        'assets/images/bangkok/korean_food_2.webp',
+      ],
+    ),
+    TravelItemVo(
+      type: TravelDetailType.text,
+      description: '파타야는 태국의 유명한 해변 도시로, 아름다운 해변과 다양한 액티비티로 유명해요.\n'
+          '파타야 해변에서 수영을 하고, 바다에서 스노클링을 했어요.\n'
+          '바다의 물이 맑고 투명해서 물고기들이 잘 보였어요.\n'
+          '스노클링을 하면서 다양한 물고기들을 볼 수 있었어요.\n'
+          '패러세일링도 하고 바나나 보트도 탔어요.\n',
+      descriptionEnglish:
+          'Pattaya is a famous beach city in Thailand, known for its beautiful beaches and various activities.\n'
+          'I swam at Pattaya Beach and went snorkeling in the sea.\n'
+          'The water was clear and transparent, allowing me to see the fish clearly.\n'
+          'I could see various fish while snorkeling.\n'
+          'I also tried parasailing and rode a banana boat.',
+    ),
+    TravelItemVo(
+      type: TravelDetailType.image,
+      imageUrls: [
+        'assets/images/bangkok/activity_1.webp',
+        'assets/images/bangkok/activity_2.webp',
+      ],
+    ),
+    TravelItemVo(
+      type: TravelDetailType.image,
+      imageUrls: ['assets/images/bangkok/activity_3.webp'],
+    ),
+    TravelItemVo(
+      type: TravelDetailType.text,
+      description: '저녁에는 터미널21 푸드코트에서 밥을 먹었어요.\n'
+          '푸팟퐁커리와 족발덮밥을 시켜 먹었어요.\n',
+      descriptionEnglish: 'I had dinner at the Terminal 21 food court.\n'
+          'I ordered Phat Phong curry and pork leg rice bowl.\n'
+          'The Phat Phong curry was delicious, and the pork leg was tender and flavorful.\n',
+    ),
+    TravelItemVo(
+      type: TravelDetailType.image,
+      imageUrls: [
+        'assets/images/bangkok/terminal21_1.webp',
+        'assets/images/bangkok/terminal21_2.webp',
+      ],
+    ),
+    TravelItemVo(
+      type: TravelDetailType.image,
+      imageUrls: [
+        'assets/images/bangkok/terminal21_3.webp',
+      ],
+    ),
+    TravelItemVo(
+      type: TravelDetailType.text,
+      description: '다섯 째날 19일에는 호텔에서 조식을 먹고 수영장을 이용했어요\n'
+          '호텔 수영장은 굉장히 넓고 깨끗했어요.\n',
+      descriptionEnglish:
+          'On the fifth day, December 19th, I had breakfast at the hotel and used the swimming pool.\n'
+          'The hotel pool was very spacious and clean.\n'
+          'I enjoyed swimming and relaxing by the poolside.\n'
+          'The weather was warm, making it a perfect day for swimming.\n',
+    ),
+    TravelItemVo(
+      type: TravelDetailType.image,
+      imageUrls: [
+        'assets/images/bangkok/hotel_food.webp',
+      ],
+    ),
+    TravelItemVo(
+      type: TravelDetailType.image,
+      imageUrls: [
+        'assets/images/bangkok/pool_1.webp',
+      ],
+    ),
+    TravelItemVo(
+      type: TravelDetailType.image,
+      imageUrls: [
+        'assets/images/bangkok/pool_2.webp',
+        'assets/images/bangkok/pool_3.webp',
+      ],
+    ),
+    TravelItemVo(
+      type: TravelDetailType.text,
+      description: '호텔에서 체크아웃을 하고 터미널 21 안에서 망고 빙수를 먹었어요.\n'
+          '태국 망고는 달고 부드러워서 맛있었어요.\n'
+          '저녁은 파타야 해변에 있는 서프앤터프라는 식당으로 갔어요.\n'
+          '팟타이와 파인애플 볶음밥, 칠리 씨푸드를 먹었어요.\n'
+          '칠리 씨푸드는 새우와 오징어, 홍합, 감자, 옥수수에 칠리 소스를 버무린 맛이였어요.\n',
+      descriptionEnglish:
+          'After checking out of the hotel, I had mango bingsu at Terminal 21.\n'
+          'Thai mango is sweet and soft, making it delicious.\n'
+          'For dinner, I went to a restaurant called Surf and Turf on Pattaya Beach.\n'
+          'I ordered Pad Thai, pineapple fried rice, and chili seafood.\n'
+          'The chili seafood was a mix of shrimp, squid, mussels, potatoes, and corn tossed in chili sauce.\n',
+    ),
+    TravelItemVo(
+      type: TravelDetailType.image,
+      imageUrls: [
+        'assets/images/bangkok/manggo.webp',
+      ],
+    ),
+    TravelItemVo(
+      type: TravelDetailType.image,
+      imageUrls: [
+        'assets/images/bangkok/sufandtuf_1.webp',
+        'assets/images/bangkok/sufandtuf_2.webp',
+      ],
+    ),
+    TravelItemVo(
+      type: TravelDetailType.text,
+      description: '저녁을 먹고 방콕 돈므앙 공항으로 돌아왔어요.\n'
+          '공항에서 새벽 2시 30분 비행기를 타고 인천으로 돌아왔어요.\n',
+      descriptionEnglish:
+          'After dinner, I returned to Don Mueang Airport in Bangkok.\n'
+          'I took a flight at 2:30 AM to return to Incheon.\n'
+          'The flight was smooth, and I arrived in Incheon early in the morning.\n'
+          'It was a memorable trip to Bangkok, filled with delicious food and beautiful sights.\n',
+    ),
+    TravelItemVo(
+      type: TravelDetailType.text,
+      description: '방콕을 여행하면서 방콕의 유적지와 문화, 음식등을 체험할 수 있어서 좋았어요.\n'
+          '음식은 한약 맛과 단 맛이 나는 경우가 많았어요.\n'
+          '파인애플과 망고를 찹쌀과 같이 먹는 게 유명한데 제 입맛에는 맞지 않았어요 ㅎㅎ\n'
+          '방콕은 하천과 강이 많아서 장마철에는 홍수가 많이 난다고 해요.\n'
+          '그래서 지하철이 지상에 있는 경우가 많아요.\n'
+          '그리고 대부분 평지로 되어 있어서 넓은 토지를 볼 수 있었어요.\n'
+          '방콕에는 마리화나 때문에 도심에 가면 박하 향이 나는 곳이 많아요.\n'
+          '태국은 왕실 국가라 왕실에 대한 존경심이 강해요.\n'
+          '왕의 사진이 곳곳에 걸려있고 왕궁에 들어갈 때 복장에 신경을 써야 해요.\n'
+          '새로운 문화를 접하면서 재미있고 신가했어요.\n'
+          '다음에는 태국의 다른 도시 치망마이로 가보고 싶어요\n',
+      descriptionEnglish:
+          'I enjoyed experiencing the ruins, culture, and food of Bangkok during my trip.\n'
+          'The food often had a herbal and sweet taste.\n'
+          'It’s famous to eat pineapple and mango with sticky rice, but it didn\'t suit my taste ㅎㅎ\n'
+          'Bangkok has many rivers and canals, so it often floods during the rainy season.\n'
+          'That’s why the subway is often above ground.\n'
+          'Most of the city is flat, allowing for wide open spaces.\n'
+          'In Bangkok, you can often smell mint in the city due to marijuana.\n'
+          'Thailand is a royal nation, so there is a strong sense of respect for the royal family.\n'
+          'Pictures of the king are hung everywhere, and you need to pay attention to your attire when entering the royal palace.\n'
+          'It was fun and interesting to encounter a new culture.\n'
+          'Next time, I want to visit Chiang Mai, another city in Thailand.\n',
     ),
   ],
 );
