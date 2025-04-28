d8139bd096e3f94a7fdba1d513d3eccafb4d7f34
ganghee <idiosauri@naver.com>
2025-04-28 20:44:16 +0900

    set gangneung data

--- lib/travel/model/gangneung_vo.dart
+++ lib/travel/model/gangneung_vo.dart
@@ -3,17 +3,133 @@ part of 'travel_detail_vo.dart';
 final TravelDetailVo _gangneungVo = TravelDetailVo(
   travelId: 4,
   items: [
-    TravelItemVo(type: TravelDetailType.text, description: '강릉 여행'),
     TravelItemVo(
-      type: TravelDetailType.image,
-      description:
-          'https://cdn.pixabay.com/photo/2016/11/29/09/08/jeju-1867280_1280.jpg',
+      type: TravelDetailType.text,
+      description: '2024년 2월 23일부터 25일까지 강릉에 다녀 왔어요.\n'
+          '동해가 보이는 연곡해변 솔향기 캠핑장에 2박을 했어요.\n',
+      descriptionEnglish: 'From February 23 to 25, 2024, I went to Gangneung.\n'
+          'I camped for two nights at Solhyanggi Campground on Yeongok Beach, which overlooks the East Sea.\n',
     ),
-    TravelItemVo(type: TravelDetailType.text, description: '강릉 여행2'),
     TravelItemVo(
       type: TravelDetailType.image,
-      description:
-          'https://cdn.pixabay.com/photo/2016/11/29/09/08/jeju-1867280_1280.jpg',
+      imageUrls: [
+        'assets/images/gangneung/camping_0.webp',
+        'assets/images/gangneung/camping_1.webp',
+      ],
+    ),
+    TravelItemVo(
+      type: TravelDetailType.text,
+      description: '캠핑은 네 번 정도 가봤지만 이렇게 추운 날 가본 건 이번이 처음이였어요.\n'
+          '텐트를 칠 때 손도 많이 시리고 눈이 많아 이동하는데 힘들었어요.\n'
+          '그래도 큰 등유 난로를 켜니 텐트 안에서 따뜻하게 있을 수 있었어요.\n',
+      descriptionEnglish:
+          'This was my first time camping in such cold weather, even though I have been camping about four times.\n'
+          'My hands were very cold while setting up the tent, and it was hard to move because of the snow.\n'
+          'However, I was able to stay warm inside the tent with a large kerosene heater.\n',
+    ),
+    TravelItemVo(
+      type: TravelDetailType.text,
+      description: '캠핑장 근처 주문진 수산시장에서 가리비를 포장해 왔어요.\n'
+          '텐트안에서 가리비와 비빔밥, 삼겹살을 먹었어요.\n',
+      descriptionEnglish:
+          'I packed scallops from a nearby seafood market in Jumunjin.\n'
+          'I ate scallops, bibimbap, and pork belly inside the tent.\n',
+    ),
+    TravelItemVo(
+      type: TravelDetailType.image,
+      imageUrls: [
+        'assets/images/gangneung/camping_2.webp',
+        'assets/images/gangneung/camping_3.webp',
+      ],
+    ),
+    TravelItemVo(
+      type: TravelDetailType.image,
+      imageUrls: [
+        'assets/images/gangneung/camping_4.webp',
+        'assets/images/gangneung/camping_5.webp',
+      ],
+    ),
+    TravelItemVo(
+      type: TravelDetailType.text,
+      description: '아침에 일어나 강릉의 형제 장칼국수를 먹으러 갔어요.\n'
+          '줄이 길어서 50분 정도 줄 서 있다가 식사를 할 수 있었어요.\n',
+      descriptionEnglish:
+          'In the morning, I went to eat Hyungje Jang Kalguksu in Gangneung.\n'
+          'I had to wait in line for about 50 minutes because the line was long.\n',
+    ),
+    TravelItemVo(
+      type: TravelDetailType.image,
+      imageUrls: [
+        'assets/images/gangneung/kalnoodle_1.webp',
+        'assets/images/gangneung/kalnoodle_2.webp',
+      ],
+    ),
+    TravelItemVo(
+      type: TravelDetailType.text,
+      description: '강릉의 중앙시장에 갔어요. 먹거리가 많았고 사람 역시 굉장히 많았어요.\n'
+          '중앙시장에서 저녁에 먹을 곰돌이 만두와 오징어 순대를 사고 만동제과 빵집에서 빵도 샀어요.\n',
+      descriptionEnglish:
+          'I went to the Central Market in Gangneung. There were many food stalls and a lot of people.\n'
+          'I bought bear-shaped dumplings and squid sundae for dinner at the Central Market, and I also bought bread at Mandong Bakery.\n',
+    ),
+    TravelItemVo(
+      type: TravelDetailType.image,
+      imageUrls: [
+        'assets/images/gangneung/market_1.webp',
+        'assets/images/gangneung/market_2.webp',
+      ],
+    ),
+    TravelItemVo(
+      type: TravelDetailType.text,
+      description: '해가 뉘엿뉘엿 질 때 쯤에 강릉 아르떼 뮤지엄에 갔어요.\n'
+          '굉장히 많은 조명으로 다양한 예술 작품을 볼 수 있었어요.\n',
+      descriptionEnglish:
+          'I went to the Gangneung Art Museum when the sun was setting.\n'
+          'I was able to see various art works with a lot of lights.\n',
+    ),
+    TravelItemVo(
+      type: TravelDetailType.image,
+      imageUrls: [
+        'assets/images/gangneung/museum_1.webp',
+        'assets/images/gangneung/museum_2.webp',
+      ],
+    ),
+    TravelItemVo(
+      type: TravelDetailType.image,
+      imageUrls: [
+        'assets/images/gangneung/museum_3.webp',
+        'assets/images/gangneung/museum_4.webp',
+      ],
+    ),
+    TravelItemVo(
+      type: TravelDetailType.text,
+      description: '캠핑장으로 되돌아 오는 길에 어제 갔던 주문진 수산시장에서 방어와 광어, 해삼, 멍게를 포장해왔어요.\n',
+      descriptionEnglish:
+          'I packed yellowtail, flounder, sea cucumber, and sea squirt from the seafood market in Jumunjin, where I went yesterday.\n',
+    ),
+    TravelItemVo(
+      type: TravelDetailType.image,
+      imageUrls: [
+        'assets/images/gangneung/dinner_1.webp',
+        'assets/images/gangneung/dinner_2.webp',
+      ],
+    ),
+    TravelItemVo(
+      type: TravelDetailType.text,
+      description: '아침에 일어나 강릉에서 유명한 순두부찌게를 먹으러 갔어요.\n'
+          '초당순두부찌게를 시켜 얼큰하고 고소했어요.\n'
+          '후식으로 순두부 젤라또는 먹었어요. 아이스크림에 고소한 두부 맛이나서 신기했어요.\n',
+      descriptionEnglish:
+          'In the morning, I went to eat the famous sundubu jjigae in Gangneung.\n'
+          'I ordered a spicy and savory sundubu jjigae.\n'
+          'For dessert, I had sundubu gelato. It was interesting because it tasted like savory tofu in ice cream.\n',
+    ),
+    TravelItemVo(
+      type: TravelDetailType.image,
+      imageUrls: [
+        'assets/images/gangneung/sontopu.webp',
+        'assets/images/gangneung/gelato.webp',
+      ],
     ),
   ],
 );
