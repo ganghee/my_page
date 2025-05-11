3b9273359afc1334445dfbf70f9436cf55047f10
ganghee <idiosauri@naver.com>
2025-05-11 15:59:13 +0900

    set loadUiImage

--- lib/travel/travel_route_painter.dart
+++ lib/travel/travel_route_painter.dart
@@ -1,4 +1,8 @@
-import 'package:flutter/widgets.dart';
+import 'dart:async';
+import 'dart:ui' as ui;
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 enum RouterType {
   first,
@@ -8,6 +12,7 @@ enum RouterType {
 class TravelRoutePainter extends CustomPainter {
   final Color color;
   final RouterType routerType;
+  ui.Image? image;
 
   TravelRoutePainter({
     required this.color,
@@ -15,18 +20,27 @@ class TravelRoutePainter extends CustomPainter {
   });
 
   @override
-  void paint(Canvas canvas, Size size) {
+  void paint(Canvas canvas, Size size) async {
     Paint paint = Paint()
-      ..color = color
+      ..color = routerType == RouterType.first ? color : color
+      ..imageFilter = ui.ImageFilter.blur(
+        sigmaX: 0.5,
+        sigmaY: 0.5,
+      )
       ..style = PaintingStyle.fill;
-    if(routerType == RouterType.first) {
-      canvas.drawPath(getFirstPath(screenSize: size), paint);
-    }else {
-      canvas.drawPath(getSecondPath(screenSize: size), paint);
+    if (routerType == RouterType.first) {
+      canvas.drawPath(_getFirstPath(screenSize: size), paint);
+    } else {
+    image = await loadUiImage('assets/images/gangneung/gangneung_pattern.webp');
+      final src =
+          Rect.fromLTWH(0, 0, image!.width.toDouble(), image!.height.toDouble());
+      final dst = Rect.fromLTWH(0, 0, size.width, size.height);
+      canvas.drawImageRect(image!, src, dst, paint);
+      canvas.drawPath(_getSecondPath(screenSize: size), paint);
     }
   }
 
-  Path getFirstPath({required Size screenSize}) {
+  Path _getFirstPath({required Size screenSize}) {
     final height = screenSize.height * 2.5;
     Path path = Path();
     path.moveTo(0, height);
@@ -43,7 +57,7 @@ class TravelRoutePainter extends CustomPainter {
     return path;
   }
 
-  Path getSecondPath({required Size screenSize}) {
+  Path _getSecondPath({required Size screenSize}) {
     final height = screenSize.height * 2.5;
 
     Path path = Path();
@@ -64,5 +78,13 @@ class TravelRoutePainter extends CustomPainter {
   @override
   bool shouldRepaint(CustomPainter oldDelegate) {
     return false;
+  }
+
+  Future<ui.Image> loadUiImage(String assetPath) async {
+    final data = await rootBundle.load(assetPath);
+    final list = Uint8List.view(data.buffer);
+    final completer = Completer<ui.Image>();
+    ui.decodeImageFromList(list, completer.complete);
+    return completer.future;
   }
 }
