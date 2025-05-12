eb18b69f8978fc86e552825537009330a16bdf09
ganghee <idiosauri@naver.com>
2025-05-12 23:51:54 +0900

    add jeju asset

--- lib/travel/model/jeju_vo.dart
+++ lib/travel/model/jeju_vo.dart
@@ -3,17 +3,403 @@ part of 'travel_detail_vo.dart';
 final TravelDetailVo _jejuVo = TravelDetailVo(
   travelId: 3,
   items: [
-    TravelItemVo(type: TravelDetailType.text, description: '제주도 여행'),
     TravelItemVo(
-      type: TravelDetailType.image,
-      description:
-          'https://cdn.pixabay.com/photo/2016/11/29/09/08/jeju-1867280_1280.jpg',
+      type: TravelDetailType.text,
+      description: '2024년 7월 9일부터 13일까지 4박 5일동안 제주도 여행을 다녀왔어요.\n'
+          '이번 제주도 여행에서는 제주 동쪽부터 반 시계 방향으로 한 바퀴 여행을 했어요.\n'
+          '13시 30분 김포 공항에서 출발해서 제주도에 도착했어요.',
+      descriptionEnglish:
+          'From July 9 to 13, 2024, I went on a 4-night, 5-day trip to Jeju Island.\n'
+          'On this trip to Jeju Island, I traveled in a counterclockwise direction starting from the east of Jeju.\n'
+          'I departed from Gimpo Airport at 1:30 PM and arrived in Jeju Island.',
     ),
-    TravelItemVo(type: TravelDetailType.text, description: '제주도 여행2'),
     TravelItemVo(
       type: TravelDetailType.image,
-      description:
-          'https://cdn.pixabay.com/photo/2016/11/29/09/08/jeju-1867280_1280.jpg',
+      imageUrls: [
+        'assets/images/jeju/airport_1.webp',
+        'assets/images/jeju/airport_2.webp',
+      ],
+    ),
+    TravelItemVo(
+      type: TravelDetailType.text,
+      description: '협재칼국수 식당에서 한치 물회와 보말죽을 먹었어요.\n'
+          '한치 물회에 우도 땅콩 가루가 뿌려져 있어서 고소한 맛과 매콤한 맛이였어요.\n'
+          '보말죽은 제주에서 많이 잡히는 고동의 종류인 보말과 고소한 참기름이 뿌려져 있어 고소했어요.\n',
+      descriptionEnglish:
+          'At Hyeopjae Kalguksu restaurant, I had squid sashimi and sea snail porridge.\n'
+          'The squid sashimi was sprinkled with peanut powder from Udo, giving it a nutty and spicy flavor.\n'
+          'The sea snail porridge was made with a type of sea snail called "bomnal" that is commonly caught in Jeju, and it was delicious with the addition of sesame oil.\n',
+    ),
+    TravelItemVo(
+      type: TravelDetailType.image,
+      imageUrls: [
+        'assets/images/jeju/bomal.webp',
+      ],
+    ),
+    TravelItemVo(
+      type: TravelDetailType.text,
+      description: '수영을 하러 판포포구에 갔어요\n'
+          '물이 차갑고 날이 쌀쌀해서 금방 나왔어요\n'
+          '숙소에 카페가 있는데 웰컴 드링크인 칵테일을 주문했어요.\n'
+          '아늑한 분위기에서 칵테일을 마셨어요\n',
+      descriptionEnglish: 'We went to Panpo Port to swim.\n'
+          'The water was cold and the weather was chilly, so we got out quickly.\n'
+          'There is a cafe in the accommodation, and I ordered a cocktail as a welcome drink.\n'
+          'I enjoyed the cocktail in a cozy atmosphere.\n',
+    ),
+    TravelItemVo(
+      type: TravelDetailType.image,
+      imageUrls: [
+        'assets/images/jeju/panpho.webp',
+        'assets/images/jeju/cocktail.webp',
+      ],
+    ),
+    TravelItemVo(
+      type: TravelDetailType.text,
+      description: '둘 째날 아침에 일어나 제주 머슴네 전복 김밥집에서 김밥을 포장해서 숙소 카페에서 먹었어요.\n'
+          '숙소 주인 분이 접시에 예쁘게 담아 주셨어요.\n'
+          '아침을 먹고 수월봉 지질로 갔어요.\n'
+          '수월봉은 제주도에서 가장 높은 해안 절벽으로, 바다와 하늘이 만나는 경치가 아름다워요.\n',
+      descriptionEnglish:
+          'On the second day, I woke up and packed gimbap from Jeju Meoseumnye Jeonbok Gimbap House and ate it at the cafe in my accommodation.\n'
+          'The owner of the accommodation beautifully arranged it on a plate.\n'
+          'After breakfast, I went to Suwolbong Geology.\n'
+          'Suwolbong is the highest coastal cliff in Jeju Island, and the scenery where the sea and sky meet is beautiful.\n',
+    ),
+    TravelItemVo(
+      type: TravelDetailType.image,
+      imageUrls: [
+        'assets/images/jeju/kimbob.webp',
+        'assets/images/jeju/suwol.webp',
+      ],
+    ),
+    TravelItemVo(
+      type: TravelDetailType.text,
+      description: '다음으로 금오름에 올라 갔어요. 우중이라 우비를 입고 금오름에 올라갔어요.\n'
+          '안개 때문에 정상에서 경치를 보기에는 좀 아쉬웠어요.\n',
+      descriptionEnglish:
+          'Next, I went up to Geumoreum. I wore a raincoat because it was raining.\n'
+          'It was a bit disappointing to see the scenery at the top due to the fog.\n',
+    ),
+    TravelItemVo(
+      type: TravelDetailType.image,
+      imageUrls: [
+        'assets/images/jeju/geumorim_1.webp',
+        'assets/images/jeju/geumorim_2.webp',
+      ],
+    ),
+    TravelItemVo(
+      type: TravelDetailType.image,
+      imageUrls: [
+        'assets/images/jeju/geumorim_3.webp',
+      ],
+    ),
+    TravelItemVo(
+      type: TravelDetailType.text,
+      description: '금오름에서 내려와 점심으로 갈치조림을 먹었어요.\n'
+          '조림이지만 찌개처럼 국물을 떠 먹을 수 있었어요.\n'
+          '미역국과 갈치 튀김, 미역 줄기, 도토리묵, 콩나물이 반찬으로 나오는데 다 맛있었습니다.\n',
+      descriptionEnglish:
+          'After coming down from Geumoreum, I had grilled cutlassfish for lunch.\n'
+          'Although it was grilled, I could eat it like a stew with soup.\n'
+          'Seaweed soup, fried cutlassfish, seaweed stems, acorn jelly, and bean sprouts were served as side dishes, and they were all delicious.\n',
+    ),
+    TravelItemVo(
+      type: TravelDetailType.image,
+      imageUrls: [
+        'assets/images/jeju/golchijorim.webp',
+      ],
+    ),
+    TravelItemVo(
+      type: TravelDetailType.text,
+      description: '점심을 먹고 제주 맥주 양조장 투어하러 갔어요.\n'
+          '맥주가 어떻게 만들어지고 어떻게 발효되는지 알 수 있었어요.\n'
+          '양조장 투어 후 제주 맥주를 시음했어요.\n'
+          '제주 위트 에일과 제주 세션 IPA를 마셨어요.\n'
+          '제주 위트 에일은 상큼한 맛이 나고 제주 세션 IPA는 쌉싸름한 맛이 나요.\n',
+      descriptionEnglish: 'After lunch, I went to the Jeju Beer Brewery tour.\n'
+          'I learned how beer is made and how it ferments.\n'
+          'After the brewery tour, I tasted Jeju beer.\n'
+          'I drank Jeju Wit Ale and Jeju Session IPA.\n'
+          'Jeju Wit Ale has a refreshing taste, while Jeju Session IPA has a bitter taste.\n',
+    ),
+    TravelItemVo(
+      type: TravelDetailType.image,
+      imageUrls: [
+        'assets/images/jeju/yangjojang_1.webp',
+        'assets/images/jeju/yangjojang_2.webp',
+      ],
+    ),
+    TravelItemVo(
+      type: TravelDetailType.image,
+      imageUrls: [
+        'assets/images/jeju/yangjojang_3.webp',
+        'assets/images/jeju/yangjojang_4.gif',
+      ],
+    ),
+    TravelItemVo(
+      type: TravelDetailType.text,
+      description: '양조장 투어를 마치고\n'
+          '제주도에 오면 흑돼지를 꼭 먹고 싶어서 저녁으로 삼겹살을 먹으러 갔어요.\n'
+          '혼디가든이라는 식당인데 인테리어가 미국 햄버거집 같은 분위기였어요.\n'
+          '직원분이 직접 구워 주셔서 정말 맛있게 먹었어요.\n ',
+      descriptionEnglish:
+          'After the brewery tour, I wanted to eat black pork, so I went to eat pork belly for dinner.\n'
+          'The restaurant is called Hondigarden, and the interior had an American burger joint vibe.\n'
+          'The staff grilled it for me, and it was really delicious.\n',
+    ),
+    TravelItemVo(
+      type: TravelDetailType.image,
+      imageUrls: [
+        'assets/images/jeju/pork_1.webp',
+        'assets/images/jeju/pork_2.webp',
+      ],
+    ),
+    TravelItemVo(
+      type: TravelDetailType.text,
+      description: '숙소로 오는 길에 제주스럽닭이라는 먹물 통닭을 포장해왔어요.\n'
+          '바삭하고 고소한 맛이 났어요. 제주맥주 양조장에서 사온 고구마 맥주와 같이 먹었어요.\n',
+      descriptionEnglish:
+          'On the way back to the accommodation, I packed black chicken from Jeju Seureopdak.\n'
+          'It had a crispy and nutty flavor. I ate it with sweet potato beer I bought from the Jeju Brewery.\n',
+    ),
+    TravelItemVo(
+      type: TravelDetailType.image,
+      imageUrls: [
+        'assets/images/jeju/chicken.webp',
+      ],
+    ),
+    TravelItemVo(
+      type: TravelDetailType.text,
+      description: '셋 째날, 아침에 일어나 오전에는 산양큰엉곶이라는 곳에 다녀왔어요.\n'
+          '산양큰엉곶은 넓은 숲길로 만든 곳이예요. 동화 속에 있을만한 크고 작은 나무 집들과\n'
+          '동물 조각상들이 곳곳에 있었어요. 안개가 많이 껴있는 날이라 운치있고 고즈넉한 분위기였어요.\n'
+          '귀여운 토끼들도 있어 근처에 당근을 사서 토끼에게 먹일 수 있어요.\n',
+      descriptionEnglish:
+          'In the morning of the third day, I visited Sanyang Geun-eonggok.\n'
+          'Sanyang Geun-eonggok is a place made with wide forest paths. There were big and small tree houses like in a fairy tale,\n'
+          'and animal sculptures everywhere. It was a charming and serene atmosphere due to the heavy fog.\n'
+          'There were also cute rabbits, and you can buy carrots nearby to feed them.\n',
+    ),
+    TravelItemVo(
+      type: TravelDetailType.image,
+      imageUrls: [
+        'assets/images/jeju/garden_1.webp',
+        'assets/images/jeju/garden_2.webp',
+      ],
+    ),
+    TravelItemVo(
+      type: TravelDetailType.image,
+      imageUrls: [
+        'assets/images/jeju/garden_3.webp',
+        'assets/images/jeju/garden_4.gif',
+      ],
+    ),
+    TravelItemVo(
+      type: TravelDetailType.text,
+      description: '다음에는 정방폭포로 갔어요. 23m 폭포가 떨어져 물소리가 정말 크고 물이 사방으로 멀리 튀었어요.\n',
+      descriptionEnglish:
+          'Next, I went to Jeongbang Waterfall. The 23m waterfall made a loud sound and splashed water everywhere.\n',
+    ),
+    TravelItemVo(
+      type: TravelDetailType.image,
+      imageUrls: [
+        'assets/images/jeju/waterfall_1.webp',
+        'assets/images/jeju/waterfall_2.webp',
+      ],
+    ),
+    TravelItemVo(
+      type: TravelDetailType.text,
+      description: '점심에는 서귀포 올레 시장으로 갔어요. 시장에는 많은 먹거리와 기념품들을 팔았어요.\n'
+          '네거리올레 식당에서 흑돼지오징어 롤까츠를 먹었는데 오징어의 쫄깃한 식감이 좋았어요.\n',
+      descriptionEnglish:
+          'For lunch, I went to Seogwipo Olle Market. The market sold many food and souvenirs.\n'
+          'I had squid roll cutlet at Negeori Olle restaurant, and I liked the chewy texture of the squid.\n',
+    ),
+    TravelItemVo(
+      type: TravelDetailType.image,
+      imageUrls: [
+        'assets/images/jeju/olle_market_1.webp',
+      ],
+    ),
+    TravelItemVo(
+      type: TravelDetailType.image,
+      imageUrls: [
+        'assets/images/jeju/olle_market_2.webp',
+      ],
+    ),
+    TravelItemVo(
+      type: TravelDetailType.text,
+      description: '식당으로 가는 길에 책계일주라는 카페에 갔어요.\n'
+          '북카페라 책장에 읽고 싶은 책을 꺼내 조용한 분위기에서 커피를 마시며 시간을 보냈어요.\n',
+      descriptionEnglish:
+          'On the way to the restaurant, I visited a cafe called Cheokgyeilju.\n'
+          'It was a book cafe, so I took out a book I wanted to read and spent time drinking coffee in a quiet atmosphere.\n',
+    ),
+    TravelItemVo(
+      type: TravelDetailType.image,
+      imageUrls: [
+        'assets/images/jeju/bookcafe_1.webp',
+        'assets/images/jeju/bookcafe_2.webp',
+      ],
+    ),
+    TravelItemVo(
+      type: TravelDetailType.text,
+      description: '숙소에 짐을 두고 회를 먹으러 온평바다 한그릇이라는 식당으로 갔어요.\n'
+          '해물모듬을 시켜서 전복과 뿔소라, 해삼, 꽃멍게, 돌멍게, 딱새우회 등 제철음식으로 된 회를 먹었어요.\n',
+      descriptionEnglish:
+          'I dropped off my luggage at the accommodation and went to a restaurant called Onpyeongbada.\n',
+    ),
+    TravelItemVo(
+      type: TravelDetailType.image,
+      imageUrls: [
+        'assets/images/jeju/onpyeongbada_1.webp',
+        'assets/images/jeju/onpyeongbada_2.webp',
+      ],
+    ),
+    TravelItemVo(
+      type: TravelDetailType.text,
+      description: '야식으로 올레 시장에서 사온 족발과 우도 땅콩 막걸리를 먹었어요.\n'
+          '땅콩 막걸리는 밤 막걸리과 비슷해 고소하고 단 맛이 강했어요.\n',
+      descriptionEnglish:
+          'For a late-night snack, I had jokbal (pig\'s trotters) and Udo peanut makgeolli that I bought from Olle Market.\n'
+          'The peanut makgeolli was similar to chestnut makgeolli, with a strong nutty and sweet flavor.\n',
+    ),
+    TravelItemVo(
+      type: TravelDetailType.image,
+      imageUrls: [
+        'assets/images/jeju/jokbal.webp',
+      ],
+    ),
+    TravelItemVo(
+      type: TravelDetailType.text,
+      description: '넷 째날, 우도 여행을 갔어요. 11시 30분 입도하고 전기차를 대여했어요.\n'
+          '서빈백사라는 곳에서 해수욕을 즐겼어요. 모처럼 비가 오지 않고 날이 무척 더웠어요.\n',
+      descriptionEnglish:
+          'On the fourth day, I went on a trip to Udo. I arrived at 11:30 AM and rented an electric car.\n'
+          'I enjoyed swimming at Seobinbaeksa. It was a rare sunny day without rain, and it was very hot.\n',
+    ),
+    TravelItemVo(
+      type: TravelDetailType.image,
+      imageUrls: [
+        'assets/images/jeju/udo_1.gif',
+      ],
+    ),
+    TravelItemVo(
+      type: TravelDetailType.image,
+      imageUrls: [
+        'assets/images/jeju/udo_2.webp',
+      ],
+    ),
+    TravelItemVo(
+      type: TravelDetailType.text,
+      description: '아이스크림을 먹고 우도봉으로 갔어요. 드넓은 초원에 말을 키우고 있었어요.\n',
+      descriptionEnglish:
+          'After eating ice cream, I went to Udo Peak. There were horses grazing in the vast pasture.\n',
+    ),
+    TravelItemVo(
+      type: TravelDetailType.image,
+      imageUrls: [
+        'assets/images/jeju/udobong_1.webp',
+        'assets/images/jeju/udobong_2.gif',
+      ],
+    ),
+    TravelItemVo(
+      type: TravelDetailType.text,
+      description: '점심으로 우도 짜장맨식당에서 해물 짬뽕과 톳 짜장면, 흑돼지 탕수육을 먹었어요.\n',
+      descriptionEnglish:
+          'For lunch, I had seafood jjampong, seaweed jjajangmyeon, and black pork sweet and sour pork at Udo Jajangman restaurant.\n',
+    ),
+    TravelItemVo(
+      type: TravelDetailType.image,
+      imageUrls: [],
+    ),
+    TravelItemVo(
+      type: TravelDetailType.text,
+      description: '배를 타기 전까지 검멀레해변과 하고수동해수욕장에서 해수욕을 했어요.\n'
+          '근처에 있는 카페에서 피너트 땅콩 아이스크림을 먹었어요.\n',
+      descriptionEnglish:
+          'I swam at Geomeolle Beach and Hagosu Beach until the ferry.\n'
+          'I had peanut ice cream at a nearby cafe.\n',
+    ),
+    TravelItemVo(
+      type: TravelDetailType.image,
+      imageUrls: [
+        'assets/images/jeju/beach_1.webp',
+        'assets/images/jeju/bean_icecream.webp',
+      ],
+    ),
+    TravelItemVo(
+      type: TravelDetailType.text,
+      description: '항구에서 배를 타고 숙소로 오는 길에 저녁에 먹을 돌문어 볶음과 고등어 회를 포장해왔어요.\n'
+          '돌문어 볶음은 매콤하고 쫄깃한 식감이 좋았어요.\n'
+          '고등어 회는 비린내가 나지 않고 고소했어요.\n',
+      descriptionEnglish:
+          'On the way back to the accommodation by ferry, I packed stir-fried octopus and mackerel sashimi for dinner.\n'
+          'The stir-fried octopus was spicy and had a chewy texture.\n'
+          'The mackerel sashimi did not have a fishy smell and was delicious.\n',
+    ),
+    TravelItemVo(
+      type: TravelDetailType.image,
+      imageUrls: [],
+    ),
+    TravelItemVo(
+      type: TravelDetailType.text,
+      description: '다섯 째날, 오전에 비밀의 숲이라는 곳으로 이동했어요.\n'
+          '산양큰엉곶과 비슷한 느낌인데 좀 더 작지만 넓은 초원과 높은 나무들이 많았어요.\n',
+      descriptionEnglish:
+          'On the fifth day, I moved to a place called Secret Forest.\n'
+          'It felt similar to Sanyang Geun-eonggok, but it was smaller with a wide pasture and many tall trees.\n',
+    ),
+    TravelItemVo(
+      type: TravelDetailType.image,
+      imageUrls: [
+        'assets/images/jeju/secret_forest_1.webp',
+        'assets/images/jeju/secret_forest_2.webp',
+      ],
+    ),
+    TravelItemVo(
+      type: TravelDetailType.text,
+      description: '점심으로 교래 손 칼국수라는 식당에서 바지락 칼국수를 먹었어요.\n'
+          '조개 육수의 담백한 맛이 좋았어요.\n',
+      descriptionEnglish:
+          'For lunch, I had clam kalguksu at a restaurant called Gyo-rae Son Kalguksu.\n'
+          'I liked the light taste of the clam broth.\n',
+    ),
+    TravelItemVo(
+      type: TravelDetailType.image,
+      imageUrls: [
+        'assets/images/jeju/kalnoodle_1.webp',
+        'assets/images/jeju/kalnoodle_2.webp',
+      ],
+    ),
+    TravelItemVo(
+      type: TravelDetailType.text,
+      description: '점심을 먹고 사려니 팜이라는 장소로 이동했어요.\n'
+          '염소와 말, 토끼, 오리, 돼지, 알파카, 설치류 등의 동물을 볼 수 있었어요.\n',
+      descriptionEnglish:
+          'After lunch, I moved to a place called Saryeoni Palm.\n'
+          'I could see animals such as goats, horses, rabbits, ducks, pigs, alpacas, and rodents.\n',
+    ),
+    TravelItemVo(
+      type: TravelDetailType.image,
+      imageUrls: [
+        'assets/images/jeju/form_1.gif',
+        'assets/images/jeju/form_2.gif',
+      ],
+    ),
+    TravelItemVo(
+      type: TravelDetailType.text,
+      description: '렌터카를 반납하고 제주 공항으로 갔어요.\n'
+          '제주도 여행을 계획할 때부터 정말 먹거리와 놀거리가 많다는 것을 느꼈어요.\n'
+          '중학교 3학년 수학여행 이후 처음으로 오는 여행이어서 스스로 계획하고 가보고 싶은 곳에 갈 수 있어서 좋았어요.\n'
+          '여행 내내 비가 내려서 아쉬웠지만 그래도 오랜만에 제주도 여행을 할 수 있어서 즐거웠어요.\n'
+          '다음에는 날씨가 좋은 날로 가서 활동적인 여행을 했으면 좋겠어요.\n',
+      descriptionEnglish: 'I return car, and go to airport\n'
+          'I felt that Jeju Island has a lot of food and activities to enjoy from the moment I started planning the trip\n'
+          'It was my first trip since my middle school trip, so I was happy to plan it myself and go to the places I wanted to visit.\n'
+          'It was a bit disappointing that it rained throughout the trip, but I still enjoyed the trip to Jeju Island after a long time.\n'
+          'Next time, I hope to go on a trip with good weather and do more active activities.\n',
     ),
   ],
 );
